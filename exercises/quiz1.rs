91f2ad73ec5cf6df5156d90cc454b1bf5b1ed9f7
Trbe <780553323@qq.com>
2024-03-11 12:34:43 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(nums: i32) -> i32 {
+    if nums > 40 {
+        nums
+    } else {
+        nums * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -31,3 +37,4 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
